cd5933e2f32ccbed41433a0873102eae54dae15d
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-13 17:12:52 -0400

    added text to item cards

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Item({item}) {
     return (
         <div className="item-card">
-            
+            <div className="item-header">
+
+                <h3>{item.title}</h3>
+                <span>Pledge ${item.cost} or more</span>
+            </div>
+            <p>{item.description}</p>
+            <div className="item-bottom">
+                <aside><span>{item.quantity}</span> left</aside>
+                <button>Select Reward</button>
+            </div>
         </div>
     )
 }
