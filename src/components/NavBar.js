b06a29b2079058473e5e6809c494ec983cfe995c
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-13 12:19:51 -0400

    added templates, variables, table of contents, and first component

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <a>  
+                About
+            </a>       
+            <a>    
+                Discover
+            </a>       
+            <a>
+                Get Started
+            </a>
+        </nav>
+    )
+}
