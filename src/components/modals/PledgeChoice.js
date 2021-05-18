3fd1dd69ef1118c8ed18aaf9d6c24dd3d718fe2a
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-05-18 11:17:21 -0400

    added styling to modal

--- src/components/modals/PledgeChoice.js
+++ src/components/modals/PledgeChoice.js
@@ -3,18 +3,22 @@ import React, {useState} from 'react'
 export default function PledgeChoice({item}) {
     const [pledge, setPledge] = useState(false)
     return (
-        <div className="pledge-choice">
-            <label 
-                className={pledge ? "highlight" : ""}
-                for={`${item.title}`}>
-                <input 
-                    onClick={() => setPledge(!pledge)}
-                    type="checkbox"
-                    value={item.title} 
-                    id={`${item.title}`}/>
-                <span>{item.title}</span> Pledge ${item.cost} or more
-            </label>
-            <aside><span>{item.quantity}</span> left</aside>
+        <div 
+            style={pledge ? {borderColor: "hsl(176, 50%, 47%)"} : {}}
+            className="pledge-choice">
+            <div className="pledge-header">
+                <label 
+                    className={pledge ? "highlight" : ""}
+                    for={`${item.title}`}>
+                    <input 
+                        onClick={() => setPledge(!pledge)}
+                        type="checkbox"
+                        value={item.title} 
+                        id={`${item.title}`}/>
+                    <span>{item.title}</span> Pledge ${item.cost} or more
+                </label>
+                <aside><span>{item.quantity}</span> left</aside>
+            </div>
             <p>{item.description}</p>
             <div 
                 style={pledge ? {} : {display: "none"}}
