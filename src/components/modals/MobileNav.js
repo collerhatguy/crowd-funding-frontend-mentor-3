891661e479c9db6e9eadd13863df5e3b7e01191b
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-10 13:19:13 -0400

    styled nav for mobile

---
+++ src/components/modals/MobileNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MobileNav() {
+    return (
+        <nav className="mobile-nav">
+            <a>About</a>       
+            <a>Discover</a>       
+            <a>Get Started</a>
+        </nav>
+    )
+}
